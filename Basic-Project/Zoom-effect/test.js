4145da6f9891d13d27760925d091897ea55b839e
AmirMohammad Khodabande <117000008+ikhodabande@users.noreply.github.com>
2023-06-01 14:11:47 +0330

    Add files via upload

---
+++ Basic-Project/Zoom-effect/test.js
@@ -0,0 +1,15 @@
+window.onload=function(){
+    const container = document.getElementById('container')
+    const img = document.querySelector('img')
+    container.addEventListener('mousemove',function(e){
+        const x = e.clientX - e.target.offsetLeft;
+        const y = e.clientY - e.target.offsetTop;
+        console.log(x,y)
+        img.style.transformOrigin = `${x}px ${y}px`;
+        img.style.transform = 'scale(2)'
+    })
+    container.addEventListener('mouseleave',function(e){
+        img.style.transformOrigin = `center center`;
+        img.style.transform = 'scale(1)'
+    })
+}
