8d8c9170687e252be2769cee0ba2be7ed867a135
Amirmohammad <amii_mhmd@yahoo.com>
2023-11-06 17:49:23 +0330

    update js

---
+++ Weather App/script.js
@@ -0,0 +1,24 @@
+const apiKey = "638d3770f32a1289f8c41b834ea30bf9";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcone = document.querySelector(".weather-icone");
+
+async function checkWeather(city){
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  var data = await(response).json();
+  
+  console.log(data)
+
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+  if(data.weather[0].main == "clouds")
+}
+
+searchBtn.addEventListener("click", ()=>{
+  checkWeather(searchBox.value);
+})
+
